4229c6f59bc14d2eaebd140f90c90f5a699108f8
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-05-22 07:28:12 +0300

    add test for linkedlist

---
+++ __test__/linked-list.test.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-undef */
+let {Node,LinkedList} = require('../Data-Structures/linked-list/linked-list.js')
+
+describe('NODE MODULE',()=>{
+  it('the node class should return an object with data and next = null',()=>{
+    expect(new Node(5)).toMatchObject({ data: 5, next: null });
+    expect(new Node('hello')).toMatchObject({ data: 'hello', next: null });
+    expect(new Node(true)).toMatchObject({ data: true, next: null });
+  });
+});
+
+describe('LINKEDLIST MODULE',()=>{
+  let linkedlist = new LinkedList();
+  linkedlist.insert(1);
+  linkedlist.insert(2);
+  linkedlist.insert(3);
+  it('Can properly insert into the linked list',()=>{
+    expect(linkedlist).toMatchObject({head:{data:1,next:{data:2,next:{data:3,next:null}}}});
+  });
+
+  it('Will return true when finding a value within the linked list that exists',()=>{
+    expect(linkedlist.includes(2)).toBeTruthy();
+    expect(linkedlist.includes(3)).toBeTruthy();
+  });
+
+  it('Will return false when searching for a value in the linked list that does not exist',()=>{
+    expect(linkedlist.includes(5)).toBeFalsy();
+    expect(linkedlist.includes('invalide')).toBeFalsy();
+  });
+
+  it('Can properly return a collection of all the values that exist in the linked list',()=>{
+    expect(linkedlist.toString()).toEqual('{1} -> {2} -> {3} -> NULL');
+    linkedlist.insert('hello');
+  });
+
+  it('Can properly insert multiple nodes into the linked list',()=>{
+    expect(linkedlist.toString()).toEqual('{1} -> {2} -> {3} -> {hello} -> NULL');
+  });
+
+  it('The head property will properly point to the first node in the linked list',()=>{
+    expect(linkedlist.head.data).toEqual(1);
+  });
+  it('Can successfully instantiate an empty linked list',()=>{
+    linkedlist.insert();
+    linkedlist.insert();
+    linkedlist.insert();
+    expect(linkedlist.toString()).toEqual('{1} -> {2} -> {3} -> {hello} -> {} -> {} -> {} -> NULL');
+  });
+
+});
