76851bfaa6192c0f1dd417560b860dc41787ee3f
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-06-01 23:25:47 +0300

    done

--- Data-Structures/linked-list/linked-list.js
+++ Data-Structures/linked-list/linked-list.js
@@ -11,11 +11,13 @@ class Node {
 class LinkedList {
   constructor(){
     this.head = null;
+    this.count = 0;
   }
   append(value=''){
     const node = new Node(value);
     if(!this.head){
       this.head = node;
+      this.count++;
       return this;
     }
     let currentNode = this.head;
@@ -23,6 +25,7 @@ class LinkedList {
       currentNode = currentNode.next;
     }
     currentNode.next = node;
+    this.count++;
     return this;
   }
   insertBefore(value,newValue){
@@ -31,12 +34,14 @@ class LinkedList {
     if(current.data === value){
       node.next = current;
       this.head = node;
+      this.count++
       return this;
     }
     while(current.next !== null){
       if(current.next.data===value){
         node.next = current.next;
         current.next = node;
+        this.count++;
         return this;
       }
       current = current.next;
@@ -50,6 +55,7 @@ class LinkedList {
       if(current.data===value){
         node.next = current.next;
         current.next = node;
+        this.count++;
         return this;
       }
       current = current.next;
@@ -80,18 +86,59 @@ class LinkedList {
     let current = this.head;
     if(current.data === value){
       this.head = current.next;
+      this.count--
       return this;
     }
     while(current.next !== null){
       if(current.next.data===value){
         current.next = current.next.next;
+        this.count--;
         return this;
       }
       current = current.next;
     }
     return 'the value you add is not in the linkedlist';
   }
-}
 
+  kthFromEnd(k){
+    let current = this.head;
+    let indexKey = this.count - k -1;
+    let i = 0;
+    console.log(indexKey)
+    while(i <= indexKey && k >= 0 ){
+      console.log(i)
+      if(i === indexKey) return current.data;
+      current = current.next;
+      i++;
+    }
+    return 'Exception';
+  }
+
+  findmiddel(){
+    let index = Math.floor(this.count / 2)
+    let current = this.head;
+    while(index <= this.count){
+      if(index === this.count) return current.data;
+      current = current.next;
+      index++;
+    }
+  }
+
+}
+// console.log( )
 module.exports.Node = Node;
 module.exports.LinkedList = LinkedList;
+// let l = new LinkedList()
+// console.log(l.append(1))
+// console.log(l.append(2))
+// console.log(l.append(3))
+// console.log(l.append(4))
+// console.log(l.append(5))
+// console.log(l.append(6))
+// console.log(l.append(7))
+// console.log(l.append(8))
+// console.log(l.delete(8))
+// console.log(l.toString())
+// console.log(l.count)
+// console.log(l.kthFromEnd(6))
+// console.log(l.findmiddel())
