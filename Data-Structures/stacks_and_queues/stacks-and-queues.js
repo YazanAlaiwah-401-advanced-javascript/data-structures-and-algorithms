950df2cbf0a7dec37a98f95b827e8ed679abade3
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-06-05 18:20:42 +0300

    make the stack and queue and the test for it

---
+++ Data-Structures/stacks_and_queues/stacks-and-queues.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-extra-boolean-cast */
+/* eslint-disable no-undef */
+/* eslint-disable no-redeclare */
+'use strict';
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  peek() {
+    if(this.isEmpty()) return 'exception';
+    return this.top.data;
+  }
+  push(item) {
+    let newNode = new Node(item);
+    if(this.top){
+      newNode.next = this.top;
+      this.top = newNode;
+    }else{
+      this.top = newNode;
+    }
+  }
+  pop() {
+    if(!!this.isEmpty()) return 'exception';
+    if(this.top.next){
+      let value = this.top.data;
+      this.top = this.top.next;
+      return value;
+    }else{
+      let value = this.top.data;
+      this.top = null;
+      return value;
+    }
+  }
+  isEmpty(){
+    return !this.top;
+  }
+}
+
+
+class Queue {
+  constructor(){
+    this.front = null;
+    this.rare = null;
+  }
+  enqueue (item) {
+    let newNode = new Node(item);
+    if(this.rare){
+      this.rare.next = newNode;
+      this.rare = newNode;
+    }else{
+      this.front = newNode;
+      this.rare = this.front;
+    }
+  }
+  dequeue(){
+    if(this.front === this.rare){
+      this.front = null;
+      this.rare = null;
+      return 'exception';
+    }
+    let temp = this.front.data;
+    this.front = this.front.next;
+    return temp;
+  }
+  peek(){
+    if(!this.isEmpty()) return this.front.data;
+    return 'exception';
+  }
+  isEmpty(){
+    return !this.front;
+  }
+}
+
+module.exports= {
+  Stack,
+  Queue,
+};
---
+++ __test__/stacks-and-queeus.test.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-undef */
+'use strict';
+let {Stack,Queue} = require('../Data-Structures/stacks_and_queues/stacks-and-queues.js');
+
+
+describe('Stack Moduel',()=>{
+  let stack = new Stack();
+ 
+  it('Can successfully push onto a stack',()=>{
+    stack.push(5);
+    expect(stack.peek()).toEqual(5);
+  });
+ 
+  it('Can successfully push multiple values onto a stack',()=>{
+    stack.push(4);
+    stack.push(3);
+    expect(stack.top).toMatchObject({data:3,next:{data:4,next:{data:5,next:null}}});
+  });
+ 
+  it('Can successfully pop off the stack',()=>{
+    expect(stack.peek()).toEqual(3);
+    stack.pop();
+    expect(stack.peek()).toEqual(4);
+  });
+ 
+  it('Can successfully peek the next item on the stack',()=>{
+    expect(stack.peek()).toEqual(4);
+  });
+
+  it('Can successfully empty a stack after multiple pops',()=>{
+    stack.pop();
+    stack.pop();
+    expect(stack.top).toEqual(null);
+  });
+ 
+  it('Can successfully instantiate an empty stack',()=>{
+    let empty = new Stack();
+    expect(empty.top).toEqual(null);
+  });
+ 
+  it('Calling pop or peek on empty stack raises exception',()=>{
+    expect(stack.pop()).toEqual('exception');
+    expect(stack.peek()).toEqual('exception');
+  });
+});
+
+describe('QUEUE MODUEL',()=>{
+  let queue = new Queue();
+  it('Can successfully enqueue into a queue',()=>{
+    queue.enqueue(1);
+    expect(queue.peek()).toEqual(1);
+  });
+
+  it('Can successfully enqueue multiple values into a queue',()=>{
+    queue.enqueue(2);
+    queue.enqueue(3);
+    expect(queue.front).toMatchObject({data:1,next:{data:2,next:{data:3,next:null}}});
+  });
+
+  it('Can successfully dequeue out of a queue the expected value',()=>{
+    expect(queue.dequeue()).toEqual(1);
+    expect(queue.peek()).toEqual(2);
+  });
+
+  it('Can successfully peek into a queue, seeing the expected value',()=>{
+    expect(queue.peek()).toEqual(2);
+  });
+
+  it('Can successfully empty a queue after multiple dequeues',()=>{
+    queue.dequeue();
+    queue.dequeue();
+    expect(queue.front).toEqual(null);
+    expect(queue.rare).toEqual(null);
+  });
+ 
+  it('Can successfully instantiate an empty queue',()=>{
+    let empty = new Queue();
+    expect(empty.rare).toEqual(null);
+    expect(empty.front).toEqual(null);
+  });
+
+  it('Calling dequeue or peek on empty queue raises exception',()=>{
+    expect(queue.peek()).toEqual('exception');
+    expect(queue.dequeue()).toEqual('exception');
+  });
+});
+
+
