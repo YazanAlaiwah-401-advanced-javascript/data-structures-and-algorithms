75984cd68913b47339face2371ac69c002b7c36c
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-06-19 15:18:01 +0300

    refactor the code

---
+++ challenges/BinarySearchTree/tree.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+'user strict';
+const Tree = require('../../Data-Structures/tree/tree.js');
+
+class BinarySearchTree extends Tree {
+  constructor(tree) {
+    super(tree);
+  }
+  breadthFirstTraversal(tree) {
+    let resulte = [];
+    let childs = [];
+    let node = tree.root;
+    if (!node) return 'the tree is empty';
+    childs.push(node);
+    while (childs.length) {
+      let tempNode = childs.shift();
+      resulte.push(tempNode.value);
+      if (tempNode.left) childs.push(tempNode.left);
+      if (tempNode.right) childs.push(tempNode.right);
+    }
+    return resulte;
+  }
+  findMaximumValue() {
+    let results = null;
+    const _traversal = (node) => {
+      if (node.left) _traversal(node.left);
+      if (node.right) _traversal(node.right);
+      results = results < node.value ? node.value : results;
+    };
+    if (this.root) {
+      _traversal(this.root);
+    }
+    return results;
+  }
+}
+
+// let t = new BinarySearchTree();
+// t.add(5);
+
+// t.add(3);
+// t.add(6);
+// t.add(9);
+// t.add(1);
+// t.add(0);
+// t.add(8);
+
+// console.log(t.breadthFirstTraversal(t));
+// console.log(t.findMaximumValue());
+module.exports = BinarySearchTree;
